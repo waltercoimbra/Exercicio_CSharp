710738a9e5f9e71acb1e8e8aed7948a19a124201
WALTER COIMBRA CORDEIRO <waltercoim@gmail.com>
2025-03-02 06:02:18 -0300

    commit modificaçoes

---
+++ ex001/ex001/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ex001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetCursorPosition(10, 4);
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write(" MEU ");
+            Console.ResetColor();
+            Thread.Sleep(600);
+            
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write("  BRASIL ");
+            Console.ResetColor();
+            Thread.Sleep(600);
+
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("  BRASILEIRO ");
+            Console.ResetColor();
+            Thread.Sleep(600);
+
+            Console.ReadKey();
+        }
+    }
+}
