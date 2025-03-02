60853928f771cfc74352a5e5e0d3a57f12505af0
WALTER COIMBRA CORDEIRO <waltercoim@gmail.com>
2025-03-02 15:17:52 -0300

    novo exercicio

---
+++ Ex003/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float num;
+            Console.Write("Digite um número Real: ");
+            float.TryParse(Console.ReadLine(), out num);
+            int n1 = (int) num;
+            int n2 = Convert.ToInt16(num);
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine($"Você digitou o valor {num:N1}");
+            Console.WriteLine($"A parte inteira do número é {n1:D}");
+            Console.WriteLine($"Arredondando, temos o número {n2:D}");
+            Console.ReadKey();
+            
+        }
+    }
+}
