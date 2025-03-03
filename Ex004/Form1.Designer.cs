7b03eb162d91652d6fa2daebd72216db828b3abc
WALTER COIMBRA CORDEIRO <waltercoim@gmail.com>
2025-03-03 06:11:54 -0300

    commiting ex004

---
+++ Ex004/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Ex004
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.panResultado = new System.Windows.Forms.Panel();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.panResultado.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Digite um número real:";
+            // 
+            // txtNum
+            // 
+            this.txtNum.Location = new System.Drawing.Point(169, 29);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(55, 20);
+            this.txtNum.TabIndex = 1;
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(249, 29);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.TabIndex = 2;
+            this.btnOk.Text = "OK";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // panResultado
+            // 
+            this.panResultado.Controls.Add(this.lbl3);
+            this.panResultado.Controls.Add(this.lbl2);
+            this.panResultado.Controls.Add(this.lbl1);
+            this.panResultado.Location = new System.Drawing.Point(63, 108);
+            this.panResultado.Name = "panResultado";
+            this.panResultado.Size = new System.Drawing.Size(261, 134);
+            this.panResultado.TabIndex = 3;
+            this.panResultado.Visible = false;
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.Location = new System.Drawing.Point(4, 13);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(44, 16);
+            this.lbl1.TabIndex = 0;
+            this.lbl1.Text = "label2";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl2.Location = new System.Drawing.Point(7, 56);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(44, 16);
+            this.lbl2.TabIndex = 1;
+            this.lbl2.Text = "label3";
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl3.Location = new System.Drawing.Point(7, 93);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(44, 16);
+            this.lbl3.TabIndex = 2;
+            this.lbl3.Text = "label4";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnOk;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(351, 289);
+            this.Controls.Add(this.panResultado);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.txtNum);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Trabalhando com números";
+            this.panResultado.ResumeLayout(false);
+            this.panResultado.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNum;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Panel panResultado;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl1;
+    }
+}
+
---
+++ Ex004/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ex004
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            float num;
+            float.TryParse(txtNum.Text, out num);
+            int n1 = (int) num;
+            int n2 = Convert.ToInt16(num);
+            lbl1.Text = $"Você digitou o valor: {num:N}";
+            
+            lbl2.Text = $"A parte inteira é: {n1:D} ";
+            
+            lbl3.Text = $"Arredondando temos: {n2:D} ";
+
+            panResultado.Visible = true;
+        }
+    }
+}
