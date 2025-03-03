7ef9601d4cea8b3884bf3604b406d78fd996e7a6
WALTER COIMBRA CORDEIRO <waltercoim@gmail.com>
2025-03-03 07:50:36 -0300

    commiting

---
+++ Ex005/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex005
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nasc;
+            Console.Write("Em que ano você nasceu? ");
+            int.TryParse(Console.ReadLine(), out nasc);
+            int ano = DateTime.Now.Year;
+            int idade = ano - nasc;
+            Console.WriteLine($"Atualmente estamo no ano de {ano}");
+            Console.WriteLine($"Se você nasceu em {nasc} vai ter {idade} anos");
+            Console.ReadKey();
+            
+        }
+    }
+}
