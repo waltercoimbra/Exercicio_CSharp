7ef14112808772e3340876e35ad4e8f3c3126e22
WALTER COIMBRA CORDEIRO <waltercoim@gmail.com>
2025-03-05 18:46:36 -0300

    commiting

---
+++ M02Ex07/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M02Ex07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float altura = 0f;
+
+            Console.Write("Digite a sua altura em metros: ");
+            float.TryParse(Console.ReadLine(), out altura);
+
+            string status = (altura <= 1.70) ? "baixa" : "alta";
+            Console.WriteLine($"Alguém com {altura} de altura é considerada uma pessoa {status}");
+        }
+    }
+}
---
+++ M02Ex08/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M02Ex08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("========= PAR OU IMPAR ===========");
+            Console.WriteLine("");
+            int num;
+            Console.Write("Digite um número: ");
+            int.TryParse(Console.ReadLine(), out num);
+
+            string tipo = (num % 2 == 0) ? "PAR" : "IMPAR";
+            Console.WriteLine($"O número {num} é {tipo}");
+            Console.ReadKey();
+        }
+    }
+}
